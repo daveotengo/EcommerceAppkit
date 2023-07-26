bef583438d8b0312bf312baef4b7041bb2f5da4b
daveotengo <30934250+daveotengo@users.noreply.github.com>
2023-07-26 19:38:44 +0000

    Initial Commit

---
+++ EcommerceAppkit/EcommerceAppkitApp.swift
@@ -0,0 +1,17 @@
+//
+//  EcommerceAppkitApp.swift
+//  EcommerceAppkit
+//
+//  Created by David Oteng on 26/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct EcommerceAppkitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
